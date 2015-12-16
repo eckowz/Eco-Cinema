5c5e354bd98041d0f13aba12f0d29090a30724a9
Guilherme Eco <guilherme_eco@hotmail.com>
2015-12-16 17:26:01 -0200

    Adicionado metodo que retorna a data e hora atual. Incluidos javaDocs.

--- Tema 1 - Venda de Ingressos no Cinema/src/util/DateUtil.java
+++ Tema 1 - Venda de Ingressos no Cinema/src/util/DateUtil.java
@@ -3,6 +3,8 @@ package util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 /**
  *
@@ -10,36 +12,88 @@ import java.util.Date;
  */
 public class DateUtil {
 
+    /**
+     * 
+     * @return Retorna a data e hora atual no formato Date
+     */
+    public static Date dataHoraAtual(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        return (calendar.getTime());
+    }
+
+    /**
+     * 
+     * @param data
+     * @return
+     * @throws ParseException 
+     */
     public static Date stringToDate(String data) throws ParseException {
         return (new SimpleDateFormat("dd/MM/yyyy").parse(data));
     }
 
+    /**
+     * 
+     * @param data
+     * @return
+     * @throws ParseException 
+     */
     public static Date stringToDateHour(String data) throws ParseException {
         return (new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(data));
     }
 
+    /**
+     * 
+     * @param data
+     * @return
+     * @throws ParseException 
+     */
     public static Date stringToHour(String data) throws ParseException {
         return (new SimpleDateFormat("HH:mm:ss").parse(data));
     }
 
+    /**
+     * 
+     * @param data
+     * @return 
+     */
     public static String hourToString(Date data) {
         return (new SimpleDateFormat("HH:mm:ss").format(data));
     }
 
+    /**
+     * 
+     * @param data
+     * @return 
+     */
     public static String dateToString(Date data) {
         return (new SimpleDateFormat("dd/MM/yyyy").format(data));
     }
 
+    /**
+     * 
+     * @param data
+     * @return 
+     */
     public static String dateHourToString(Date data) {
         SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         String dataString = formatador.format(data);
         return (dataString);
     }
 
+    /**
+     * 
+     * @param data
+     * @return 
+     */
     public static boolean verificaData(String data) {
         return (data.matches("\\d{2}/\\d{2}/\\d{4}"));
     }
 
+    /**
+     * 
+     * @param data
+     * @return 
+     */
     public static boolean verificaDataHora(String data) {
         return (data.matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
     }
