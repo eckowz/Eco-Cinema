b938b81c99b673571877cf01dca4025fb481218c
Guilherme Eco <guilherme_eco@hotmail.com>
2015-12-16 16:07:37 -0200

    Incluido javaDocs.

--- Tema 1 - Venda de Ingressos no Cinema/src/banco/ConnectionFactory.java
+++ Tema 1 - Venda de Ingressos no Cinema/src/banco/ConnectionFactory.java
@@ -8,32 +8,33 @@ import java.util.logging.Logger;
 import view.PrintUtil;
 
 /**
- * 
+ *
  * @author Eckowz
  */
 public class ConnectionFactory {
+
     private final static String HOST = "localhost";
     private final static String PORT = "5432";
     private final static String BD = "eco_cinema";
-    private final static String URL = "jdbc:postgresql://"+HOST+":"+PORT+"/"+BD;
+    private final static String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + BD;
     private final static String USUARIO = "postgres";
     private final static String SENHA = "1234";
-    
+
     /**
-     * 
-     * @return - retorna se a conexão com o banco foi realizada com sucesso
+     *
+     * @return Retorna se a conexão com o banco foi realizada com sucesso
      */
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection conexao = null;
         try {
             Class.forName("org.postgresql.Driver");
             conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
-            PrintUtil.printMessageErro(null, null, ex);
+            PrintUtil.printMessageErro(null, ex);
         }
-        return(conexao);
+        return (conexao);
     }
-    
+
 }
